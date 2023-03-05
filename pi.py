fd3cca52a33e444714edf6f2963a014ff32bc2cc
lee <lee.k.clarke@gmail.com>
2023-03-05 18:20:36 -0500

    updated to remove comment

---
+++ pi.py
@@ -0,0 +1,10 @@
+class Pi:
+    def __init__(self, name, ip, network, tag, timestamp, status= 'GREEN'):
+        self.name = name
+        self.ip = ip
+        self.network = network
+        self.tag = tag
+        self.timestamp = timestamp
+        self.status = status
+
+        
